2220e9d58bfb76fba08b7baa0f797a486c9b0525
alenushka <abraneuskaya@gmail.com>
2020-10-31 05:39:58 -0400

    DZ 1 DZ 2

---
+++ src/DZ1/Task4.java
@@ -0,0 +1,21 @@
+package DZ1;
+import java.util.Scanner;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println(" Enter number: ");
+
+            int a = 300;
+            int b = scan.nextInt();
+
+            if (b < 0.9 * a || b > 1.1 * a) {
+                System.out.println( "Number: " + b + " 10% more from " + a) ;
+
+            }else {
+                System.out.println(" Number: " + b + " 10% less from" + a);
+            }
+    }
+
+}
